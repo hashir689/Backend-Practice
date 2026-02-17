75f01240aae6787523b52ec0820da465b260c78d
hashir689 <hashirhussain689@gmail.com>
2026-02-17 18:06:08 +0500

    Schema Update inclue JWT etc

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME = "BackendPractice"
+export const DB_NAME = "BackendPractice";
--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-
 import { configDotenv } from "dotenv";
 import mongoose from "mongoose";
-
 
 import connectDB from "./Db/Connection.js";
 import { app } from "./app.js";
 
 configDotenv();
 
-
-
-
-connectDB().then(()=>{
-   app.listen(process.env.PORT,()=>{
-console.log("Server is Running");
-   })
-}).catch((err)=>{
-console.log("connection failed" , err)
-});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Server is Running");
+    });
+  })
+  .catch((err) => {
+    console.log("connection failed", err);
+  });
